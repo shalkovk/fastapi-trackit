b2037ba328c4ba15897bfd5a6401cafaf2f00ada
shalkovk <kshalkovskiy@gmail.com>
2025-06-23 21:24:17 +0500

    added notify utility & fixed sending queu to rabbitmq

--- backend/tracking_service/tasks/update_parcels.py
+++ backend/tracking_service/tasks/update_parcels.py
@@ -3,8 +3,7 @@ from db.database import get_db, SessionLocal
 from db.models.models import Parcel
 from sqlalchemy.future import select
 from utils.track24 import get_parcel_status
-import httpx
-from datetime import datetime, timezone
+from utils.notify import send_status_update
 
 
 @celery_app.task(name="tasks.update_parcels.update_parcels")
@@ -26,6 +25,8 @@ async def _update_parcels_async():
                         parcel.status = parcel_data["status"]
                         parcel.last_update = parcel_data["last_update"]
                         parcel.carrier = parcel_data["carrier"]
+
+                        await send_status_update(parcel)
                 except Exception as e:
                     print(
                         f"Ошибка при обновлении {parcel.tracking_number}: {str(e)}")
---
+++ backend/tracking_service/utils/notify.py
@@ -0,0 +1,33 @@
+import aio_pika
+import json
+import os
+from dotenv import load_dotenv
+from pathlib import Path
+
+env_path = Path(__file__).resolve().parents[1] / ".env"
+
+load_dotenv(dotenv_path=env_path)
+
+RABBITMQ_URL = os.getenv("RABBITMQ_URL", "amqp://guest:guest@rabbitmq/")
+QUEUE_NAME = os.getenv("QUEUE_NAME", "parcel_status_updates")
+
+
+async def send_status_update(parcel):
+    connection = await aio_pika.connect_robust(RABBITMQ_URL)
+    channel = await connection.channel()
+    queue = await channel.declare_queue(QUEUE_NAME, durable=True)
+
+    message_body = {
+        "telegram_id": parcel.owner_telegram_id,
+        "tracking_number": parcel.tracking_number,
+        "new_status": parcel.status,
+        "carrier": parcel.carrier or "Unknown",
+        "last_update": str(parcel.last_update)
+    }
+
+    await channel.default_exchange.publish(
+        aio_pika.Message(body=json.dumps(message_body).encode()),
+        routing_key=QUEUE_NAME
+    )
+
+    await connection.close()
